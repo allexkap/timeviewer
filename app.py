61cbb64432218f579d651e7b3758304fea521729
allexkap <aekpstn@gmail.com>
2024-02-18 15:06:43 +0300

    add date form

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
+from datetime import date
+
 import numpy as np
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    try:
+        start = date.fromisoformat(request.args["from"])
+        end = date.fromisoformat(request.args["to"])
+        assert start < end
+    except:
+        end = date.today()
+        start = end.replace(year=end.year - 1)
+
     np.random.seed(0)
-    data = np.random.random((7, 40))
-    return render_template("index.html", data=data)
+    table = np.random.random((7, 40))
+
+    return render_template("index.html", table=table, dates=(start, end))
 
 
 if __name__ == "__main__":
