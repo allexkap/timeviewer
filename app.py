0dc9a3d4a22ac9558fc79448babd2c3c42e3ca4d
allexkap <aekpstn@gmail.com>
2024-02-18 22:06:02 +0300

    add app select

--- app.py
+++ app.py
@@ -6,21 +6,23 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/', methods=['get', 'post'])
 def index():
     try:
-        start = date.fromisoformat(request.args["from"])
-        end = date.fromisoformat(request.args["to"])
+        start = date.fromisoformat(request.form['from'])
+        end = date.fromisoformat(request.form['to'])
         assert start < end
     except:
         end = date.today()
         start = end.replace(year=end.year - 1)
 
+    apps = (('app0', 'app1', 'app2'), request.form.get('app', ''))
+
     np.random.seed(0)
     table = np.random.random((7, 40))
 
-    return render_template("index.html", table=table, dates=(start, end))
+    return render_template('index.html', table=table, apps=apps, dates=(start, end))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
